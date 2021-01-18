1758d749c04512bfa55cd204e43fce524aa5b2ad
Giovana <f_gi@live.com>
2021-01-18 16:07:58 -0300

    v-for e :key para exibir dados de api

--- src/components/FetchPrevent.vue
+++ src/components/FetchPrevent.vue
@@ -1,21 +1,30 @@
 <template lang="html">
     <div class="wrap">
-        <h2> Pokemons: </h2>
+        <h2> Pokemons abilities: </h2>
         <ul>
             <li> <a @click.prevent="puxarAcao" href="https://pokeapi.co/api/v2/pokemon/ditto"> Ditto </a> </li>
             <li> <a @click.prevent="puxarAcao" href="https://pokeapi.co/api/v2/pokemon/pikachu"> Pikachu </a> </li>
             <li> <a @click.prevent="puxarAcao" href="https://pokeapi.co/api/v2/pokemon/bulbasaur"> Bulbasaur </a> </li>
-        </ul>    
-        <div>
+        </ul>
+        <div  v-for="(value) in habilidades" :key="key+value">
+            <!-- <p>index:{{index}}</p>
+            <p>key:{{key}}</p>
+            <p>value:{{value}}</p> -->
+            <p>{{value.ability.name}}</p>
+        </div>        
+        <!-- <div>
+            <p>{{habilidades}}</p> 
+            <div class="rep"> </div>
             <p>{{pokemon}}</p>
-        </div>    
+        </div>     -->
     </div>        
 </template>
 
 <script>
 export default {
     data: () => ({
-        pokemon: {}
+        pokemon: {},
+        habilidades: {}
     }),
     methods:{
         puxarAcao(event){
@@ -24,12 +33,17 @@ export default {
             .then(response => response.json())
             .then(response => {
                 this.pokemon=response;
-            })
+                this.habilidades = this.pokemon.abilities;
+            });
+            
         }
     }
 }
 </script>
 
 <style lang="stylus">
-
+.rep
+    width 100px
+    height 10px
+    background-color pink
 </style>
