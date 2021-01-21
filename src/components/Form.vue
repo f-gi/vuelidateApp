819a2f519970b49247e5de006aa0a851eef79ced
Giovana <f_gi@live.com>
2021-01-21 18:16:51 -0300

    Ajustes BEM

--- src/components/Form.vue
+++ src/components/Form.vue
@@ -5,74 +5,75 @@
         <div v-if="sucess">
             <p> Conta criada com sucesso! </p>
         </div>
-        <form @submit.prevent="submit" v-else>
+        <form @submit.prevent="submit" class="formulario" v-else>
             <fieldset>
-                <div class="input">
+                <div class="formulario__input">
                     <label for="nome">Nome*</label>
                     <input 
                         v-model="$v.criarConta.name.$model" 
                         type="text"
                         id="nome" 
                         @change="$v.criarConta.name.$touch()" 
-                        :class="{'input__focus':$v.criarConta.name.$error}"
+                        :class="{'formulario__input--focusError':$v.criarConta.name.$error}"
                     />
-                    <span v-if="$v.criarConta.name.minLength"> Seu nome precisa ter no minimo {{$v.criarConta.name.$params.minLength.min}} letras.</span>
+                    <span v-if="$v.criarConta.name.$error"> Seu nome precisa ter no minimo {{$v.criarConta.name.$params.minLength.min}} letras.</span>
+                {{$v.criarConta.name.minLength}}
                 </div> 
-                <div class="input">
+                <div class="formulario__input">
                     <label for="email">E-mail*</label>
                     <input 
                         v-model="$v.criarConta.mail.$model" 
                         type="email" 
-                        id="email" 
-                        :class="{'input__focus':$v.criarConta.mail.$error}"
+                        id="email"
+                        :class="{'formulario__input--focusError':$v.criarConta.mail.$error}"
                     />
                     <span v-if="$v.criarConta.mail.$error"> Este E-mail é invalido </span>
                 </div>
-                <div class="input">
+                <div class="formulario__input">
                     <label for="senha">Senha*</label>
                     <input 
                         v-model="$v.criarConta.password.$model" 
                         type="password" 
                         id="senha" 
-                        :class="{'input__focus':$v.criarConta.password.$error}"
+                        :class="{'formulario__input--focusError':$v.criarConta.password.$error}"
                     />
-                    <span v-if="$v.criarConta.password.minLength"> A senha precisa ter entre {{$v.criarConta.password.$params.minLength.min}} e {{$v.criarConta.password.$params.maxLength.max}} caracteres </span>
+                    <span v-if="$v.criarConta.password.$error"> A senha precisa ter entre {{$v.criarConta.password.$params.minLength.min}} e {{$v.criarConta.password.$params.maxLength.max}} caracteres </span>
 
                 </div>
-                <div class="input">
+                <div class="formulario__input">
                     <label for="confsenha">Confirme sua senha*</label>
                     <input 
                         v-model="$v.criarConta.conf_password.$model" 
                         type="password" 
                         id="confsenha" 
-                        :class="{'input__focus':$v.criarConta.conf_password.$error}"/>
+                        :class="{'formulario__input--focusError':$v.criarConta.conf_password.$error}"/>
                     <span v-if="$v.criarConta.conf_password.$error"> As senhas informadas não coincidem </span>
                 </div>
-                <div class="input">
-                    <label class="input__check" for="receberEmail">Aceitar receber e-mails?</label>
+                <div class="formulario__input">
+                    <label class="formulario__input--check" for="receberEmail">Aceitar receber e-mails?</label>
                     <input 
-                        class="input__check" 
+                        class="formulario__input--check" 
                         type="checkbox" 
                         v-model="$v.criarConta.receberEmail" 
                         id="receberEmail" 
                     />
                 </div>    
-                <div class="input">
-                    <label class="input__check" for="termos">Aceita os Termos e Condições?</label>
+                <div class="formulario__input">
+                    <label class="formulario__input--check" for="termos">Aceita os Termos e Condições?</label>
                     <input 
-                        class="input__check" 
+                        class="formulario__input--check" 
                         type="checkbox" 
                         v-model="$v.criarConta.termos" 
                         id="termos" 
                     />
                 </div>    
-                <div class="input">
+                <div class="formulario__input">
                     <label for="msg">Mensagem:</label>
                     <textarea v-model="$v.criarConta.message" id="msg"></textarea>
                 </div> 
                 <!-- @click.prevent="$v.$touch()" não precisa disso -->
                 <!-- <button  class="btn" @click.prevent="$v.$touch()">Criar</button> -->
-                <button  class="btn" type="submit" :disabled="$v.$invalid">Criar</button>
+                <button  class="formulario__btn" type="submit" :disabled="$v.$invalid">Criar</button>
                 <!-- @click.prevent="click", o prevent do submit fica no form, não no botão em si -->
                 <!-- <Botao :type="submit"/> usando emit para o type dinamico em componente -->
             </fieldset>
@@ -138,7 +139,7 @@ export default {
 @import '../assets/stylus/general.styl' 
 
 .container   
-    form 
+    .formulario 
         font-family $font-primary
         font-size $subtitle-desk
         fieldset
@@ -157,7 +158,7 @@ export default {
             font-size $subtitle-desk  
         textarea
             height 85px
-        .input
+        .formulario__input
             margin 15px auto
             width 80%
             @media screen and (min-width: 600px)
@@ -165,14 +166,18 @@ export default {
             span
                 font-size $legend
                 color $color-primary 
-            .input__focus
+            input
+                outline none                
+            input:focus 
                 border-color $color-primary
-            .input__check
+            .formulario__input--focusError
+                border-color $color-primary
+            .formulario__input--check
                 margin 0 0 8px 0
                 @media screen and (min-width: 600px)
                     display inline
-                    width 20px     
-        .btn 
+                    width 20px  
+        .formulario__btn 
             background-color $color-primary
             font-family $font-primary
             font-size $subtitle-desk
